7d1b8b7d930fcd2281edc9270345db276598bddd
nioe <patrickmitchell07@proton.me>
2025-02-21 17:13:44 -0500

    init commit

---
+++ api/src/lib.rs
@@ -0,0 +1,28 @@
+pub mod auth_service;
+
+use derive_more::From;
+// server::run([auth_service, messaging_service])
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug, From)]
+pub enum Error {
+    // -- fs
+    LimitTooHigh { actual: usize, max: usize },
+
+    // -- Externals
+    #[from]
+    Io(std::io::Error), // as an example
+
+    #[from]
+    DB(surrealdb::Error)
+}
+
+// Note: Implement Display as debug, for Web and app error, as anyway those errors will need to be streamed as JSON probably 
+//       to be rendered for the end user. 
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}

7d2b851960463b811d8fbf8d60c7cef52823582a
nioe <patrickmitchell07@proton.me>
2025-03-02 00:46:54 -0500

    reformat api

--- api/src/server/mod.rs
+++ api/src/server/mod.rs
@@ -1,7 +1,14 @@
+use axum::Router;
 
+use crate::auth_service;
 
 
 pub async fn run_server() -> std::io::Result<()> {
-    
+
+    let app_data = Data::new(firebase_auth);
+
+    let auth_service_routes = Router::new();
+        
     Ok(())
 }
+

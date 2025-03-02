6872b0c21b68e277435a69ec02d06f8a293e3abb
StoweWilson <ifunkychunky@gmail.com>
2025-03-02 00:51:34 -0500

    1/2 through homepage (i think)

---
+++ client/src/routes/home/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { page } from '$app/stores';
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input,Avatar } from 'flowbite-svelte';
+    $: activeUrl = $page.url.pathname;
+    let activeClass = 'text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent';
+    let nonActiveClass = 'text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
+    import { SpeedDial, SpeedDialButton } from 'flowbite-svelte';
+    import { ShareNodesSolid, SearchOutline, PhoneSolid, UserAddSolid } from 'flowbite-svelte-icons';
+  
+
+</script>
+  
+  <Navbar>
+    <NavBrand href="/">
+      <img src="" class="me-3 h-6 sm:h-9" alt="image here" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Message App</span>
+    </NavBrand>
+    
+    <div class="flex items-right md:order-2">
+        <Avatar id="avatar-menu" src="/images/profile-picture-3.webp" />
+    </div>
+    <div class="flex md:order-2">
+        <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-hidden focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+          <SearchOutline class="w-5 h-5" />
+        </Button>
+        <div class="hidden relative md:block">
+            <div class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none">
+              <SearchOutline class="w-4 h-4" />
+            </div>
+            <Input id="search-navbar" class="ps-10" placeholder="Search..." />
+        </div>
+    </div>
+  </Navbar>
+
+  <SpeedDial color="blue" defaultClass="absolute end-10 bottom-10">
+    <SpeedDialButton name="Share">
+      <ShareNodesSolid class="w-6 h-6" />
+    </SpeedDialButton>
+    <SpeedDialButton name="New Contact">
+      <UserAddSolid class="w-6 h-6" />
+    </SpeedDialButton>
+    <SpeedDialButton name="Call">
+      <PhoneSolid class="w-6 h-6" />
+    </SpeedDialButton>
+  </SpeedDial>
+
+  <slot></slot>
---
+++ client/src/routes/home/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Listgroup, ListgroupItem, Avatar } from 'flowbite-svelte';
+    import { TrashBinSolid } from 'flowbite-svelte-icons';
+  </script>
+  
+  <Listgroup active class="self-centerw-100">
+    <h3 class="p-1 text-center text-xl font-medium text-gray-900 dark:text-white">User list</h3>
+    <ListgroupItem class="text-base font-semibold gap-2">
+      <Avatar src="/images/profile-picture-1.webp" size="xs" />Jese Leos
+    </ListgroupItem>
+    <ListgroupItem class="text-base font-semibold gap-2">
+      <Avatar src="/images/profile-picture-2.webp" size="xs" />Robert Gouth
+    </ListgroupItem>
+    <ListgroupItem class="text-base font-semibold gap-2">
+      <Avatar src="/images/profile-picture-3.webp" size="xs" />Bonnie Green
+    </ListgroupItem>
+    <a href="/" class="flex items-center p-3 text-sm font-medium text-red-600 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-red-500 hover:underline rounded-b-lg">
+      <TrashBinSolid class="w-6 h-6 ms-1 me-2" />
+      Delete user
+    </a>
+  </Listgroup>

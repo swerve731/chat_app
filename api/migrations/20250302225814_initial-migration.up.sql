2a3e7fe3d07106afc7dd6960cc737b5824f67570
StoweWilson <ifunkychunky@gmail.com>
2025-03-02 23:01:31 -0500

    contacts

--- api/migrations/20250302225814_initial-migration.up.sql
+++ api/migrations/20250302225814_initial-migration.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
+    pass TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
 
 CREATE TABLE messages (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY,
     sender_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     receiver_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     content TEXT NOT NULL,

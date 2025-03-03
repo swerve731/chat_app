a123019d82fd1104131496b4814d3efecd772753
pogthoin <patrickmitchell07@proton.me>
2025-03-02 23:04:51 -0500

    start user auth functions note: not encryption yet

--- api/migrations/20250302225814_initial-migration.down.sql
+++ api/migrations/20250302225814_initial-migration.down.sql
@@ -1,3 +1,3 @@
 -- Add down migration script here
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
--- api/migrations/20250302225814_initial-migration.up.sql
+++ api/migrations/20250302225814_initial-migration.up.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL 
 );
 
 
 CREATE TABLE messages (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY,
     sender_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     receiver_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     content TEXT NOT NULL,
-    sent_at TIMESTAMP DEFAULT NOW()
+    sent_at TIMESTAMP NOT NULL
 );
